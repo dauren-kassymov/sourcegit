8c6aa561f16180e8f459fab4b4a7365b90bfb13a
daurenKa <dauren.kass@gmail.com>
2024-12-20 11:31:22 +0500

    d

--- src/SourceGit/Views/GitLogs.axaml.cs
+++ src/SourceGit/Views/GitLogs.axaml.cs
@@ -4,6 +4,8 @@ using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
+using SourceGit.Models;
+
 namespace SourceGit.Views
 {
     public partial class GitLogs : Window
@@ -21,6 +23,16 @@ namespace SourceGit.Views
         private void CloseWindow(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void OnGridDoubleTapped(object sender, TappedEventArgs e)
+        {
+            if (e.Source is TextBlock textBlock && 
+                textBlock.DataContext is GitLog log)
+            {
+                // Copy the message to clipboard
+                Clipboard?.SetTextAsync(log.Message);
+            }
         }
     }
 }
